55ae3ee6871ab4806a473cad9ae1a919bd5ccab4
Elish84 <89509077+Elish84@users.noreply.github.com>
2026-01-04 23:26:04 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// IMPORTANT:
+// 1) ודא שהפעלת ב-Firebase Console את Authentication (Email/Password)
+// 2) ודא שהפעלת Firestore Database
+// 3) אפשר להחליף את הקונפיג הזה לשלך (כמו ששלחת)
+
+window.GANTT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDy_Rg6wZOjqPlkHyYNV73OHrjpYECYclo",
+  authDomain: "gantt-72b70.firebaseapp.com",
+  projectId: "gantt-72b70",
+  storageBucket: "gantt-72b70.firebasestorage.app",
+  messagingSenderId: "529145680876",
+  appId: "1:529145680876:web:d5f9c637bd8f3bf64926c9",
+  measurementId: "G-DY7ETNBLF6"
+};
